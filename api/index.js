169427628f8f9124c250230ae0a1baac553457f3
shahzad11khan <61977658+shahzad11khan@users.noreply.github.com>
2024-12-09 14:35:05 +0500

    tinypuse

--- api/index.js
+++ api/index.js
@@ -1,35 +1,20 @@
-const { ApolloServer, gql } = require("apollo-server-micro");
+const { ApolloServer } = require("apollo-server-micro");
+const typeDefs = require("../graphql/typeDefs");
+const resolvers = require("../graphql/resolvers");
+const context = require("../graphql/context");
 
-// Define your schema
-const typeDefs = gql`
-  type Query {
-    hello: String
-  }
-`;
-
-// Define your resolvers
-const resolvers = {
-  Query: {
-    hello: () => "Hello, world!",
-  },
-};
-
-// Create Apollo Server instance
 const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  cache: "bounded", // Use bounded cache
-  persistedQueries: false, // Disable persisted queries for security
+    typeDefs,
+    resolvers,
+    context,
+    cache: "bounded",
+    persistedQueries: false,
 });
 
-// Ensure the server starts before creating the handler
 const startServer = server.start().then(() => {
-  module.exports = server.createHandler({
-    path: "/api",
-  });
+    module.exports = server.createHandler({ path: "/api" });
 });
 
-// Export the handler
 module.exports = async (req, res) => {
-  await startServer;
+    await startServer;
 };
