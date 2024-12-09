6af6578c410ab4bf2af3d35ec311be4a4e9eb8ed
shahzad11khan <61977658+shahzad11khan@users.noreply.github.com>
2024-12-09 14:26:15 +0500

    tinypuse

--- api/index.js
+++ api/index.js
@@ -1,14 +1,35 @@
-const { ApolloServer, gql } = require('apollo-server-micro');
+const { ApolloServer, gql } = require("apollo-server-micro");
+
+// Define your schema
 const typeDefs = gql`
   type Query {
     hello: String
   }
 `;
+
+// Define your resolvers
 const resolvers = {
   Query: {
-    hello: () => 'Hello world!',
+    hello: () => "Hello, world!",
   },
 };
-const apolloServer = new ApolloServer({ typeDefs, resolvers });
 
-module.exports = apolloServer.createHandler();
+// Create Apollo Server instance
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  cache: "bounded", // Use bounded cache
+  persistedQueries: false, // Disable persisted queries for security
+});
+
+// Ensure the server starts before creating the handler
+const startServer = server.start().then(() => {
+  module.exports = server.createHandler({
+    path: "/api",
+  });
+});
+
+// Export the handler
+module.exports = async (req, res) => {
+  await startServer;
+};
