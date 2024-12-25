38171cb8bd2646474cf134356777ab9450cfe6f7
shahzad11khan <zarakali401@gmail.com>
2024-12-25 14:17:14 +0500

    change

--- src/middleware/uploadToCloudinary.js
+++ src/middleware/uploadToCloudinary.js
@@ -1,4 +1,5 @@
 const cloudinary = require('cloudinary').v2;
+const fs = require('fs');
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
@@ -6,15 +7,18 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-const uploadToCloudinary = (base64Image) => {
+const uploadToCloudinary = (stream, filename) => {
   return new Promise((resolve, reject) => {
-    cloudinary.uploader.upload(base64Image, { resource_type: "auto" }, (error, result) => {
-      if (error) {
-        reject(error);
-      } else {
+    const uploadStream = cloudinary.uploader.upload_stream(
+      { resource_type: 'auto', public_id: filename },
+      (error, result) => {
+        if (error) {
+          return reject(error);
+        }
         resolve(result);
       }
-    });
+    );
+    stream.pipe(uploadStream);  // Pipe the file stream to Cloudinary
   });
 };
 
