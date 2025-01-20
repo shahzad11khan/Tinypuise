4f26ed36f1b96bf7650e8e54a63eefe75c65e38d
ShaX00 khan <61977658+shahzad11khan@users.noreply.github.com>
2025-01-20 14:26:14 +0500

    token expire time 6m

--- src/resolvers/userresolvers.js
+++ src/resolvers/userresolvers.js
@@ -215,7 +215,7 @@ const resolvers = {
         throw new Error('Invalid email or password');
       }
 
-      const token = jwt.sign({ userId: user.id,parentName: user.name,email: user.email  }, JWT_SECRET, { expiresIn: '1y' });
+      const token = jwt.sign({ userId: user.id,parentName: user.name,email: user.email  }, JWT_SECRET, { expiresIn: '6m' });
       return {
         token,
         user,
