ed82d73347b46064cdca47c142651e924babbe65
shahzad11khan <zarakali401@gmail.com>
2025-01-30 16:11:32 +0500

    add articles section

--- src/schema.js
+++ src/schema.js
@@ -7,13 +7,15 @@ const feedTypeDef = require('./typedefs/feedingTypeDef');
 const diaperTypeDef = require('./typedefs/diaperTypeDef');
 const sleepTypeDef = require('./typedefs/sleepTypeDef');
 const growthTypeDef = require('./typedefs/growthTypeDef');
+const articlesTypeDef = require('./typedefs/articlesTypeDef');
 const feedResolver = require('./resolvers/feedingResolver');
 const diaperResolver = require('./resolvers/diaperResolver');
 const sleepResolver = require('./resolvers/sleepResolver');
 const growthResolver = require('./resolvers/growthResolver');
+const articlesResolver = require('./resolvers/articlesResolver');
 
 
-const typeDefs = mergeTypeDefs([userTypeDef, babyInfoTypeDef,feedTypeDef,diaperTypeDef,sleepTypeDef,growthTypeDef]);
-const resolvers = mergeResolvers([userResolver, babyInfoResolver,feedResolver,diaperResolver,sleepResolver,growthResolver]);
+const typeDefs = mergeTypeDefs([userTypeDef, babyInfoTypeDef,feedTypeDef,diaperTypeDef,sleepTypeDef,growthTypeDef,articlesTypeDef]);
+const resolvers = mergeResolvers([userResolver, babyInfoResolver,feedResolver,diaperResolver,sleepResolver,growthResolver,articlesResolver]);
 
 module.exports = { typeDefs, resolvers };
